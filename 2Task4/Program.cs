0b005b46ded9e50d32658820035699ca41125e10
Anastasiia <martishka85@list.ru>
2023-06-13 12:03:06 +0300

    2Task4

---
+++ 2Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+// 5, 25 -> да 
+// -4, 16 -> да 
+// 25, 5 -> да 
+// 8, 9 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse (Console.ReadLine()!);
+int x = 0; //квадрат меньшего числа
+if (a > b)
+{
+    x = b * b;
+    if (x == a)
+    {
+        Console.WriteLine($" {a}, {b} -> да");
+    }
+}
+else if (a < b)
+{
+    x = a * a;
+    if (x == b)
+    {
+        Console.WriteLine($" {a}, {b} -> да");
+    }
+    else 
+    {
+        Console.WriteLine($" {a}, {b} -> нет");
+    }
+}
