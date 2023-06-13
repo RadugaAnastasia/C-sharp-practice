f9f661a3d13cbe50e69311b85cb9eb9705e88bc8
Anastasiia <martishka85@list.ru>
2023-06-13 12:26:23 +0300

    2Task4

--- 2Task4/Program.cs
+++ 2Task4/Program.cs
@@ -5,29 +5,46 @@
 // 25, 5 -> да 
 // 8, 9 -> нет
 
-Console.Clear();
-Console.WriteLine("Введите первое число: ");
-int a = int.Parse (Console.ReadLine()!);
-Console.WriteLine("Введите второе число: ");
-int b = int.Parse (Console.ReadLine()!);
-int x = 0; //квадрат меньшего числа
-if (a > b)
+// Console.Clear();
+// Console.WriteLine("Введите первое число: ");
+// int a = int.Parse (Console.ReadLine()!);
+// Console.WriteLine("Введите второе число: ");
+// int b = int.Parse (Console.ReadLine()!);
+// int x = 0; //квадрат меньшего числа
+// if (a > b)
+// {
+//     x = b * b;
+//     if (x == a)
+//     {
+//         Console.WriteLine($" {a}, {b} -> да");
+//     }
+// }
+// else if (a <= b)
+// {
+//     x = a * a;
+//     if (x == b)
+//     {
+//         Console.WriteLine($" {a}, {b} -> да");
+//     }
+//     else 
+//     {
+//         Console.WriteLine($" {a}, {b} -> нет");
+//     }
+// }
+
+Console.WriteLine("Введите число:");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+string res = "";
+if(numberTwo * numberTwo != numberOne && numberOne * numberOne != numberTwo)
 {
-    x = b * b;
-    if (x == a)
-    {
-        Console.WriteLine($" {a}, {b} -> да");
-    }
+    res = ($" {a}, {b} -> нет");
 }
-else if (a < b)
+
+else 
 {
-    x = a * a;
-    if (x == b)
-    {
-        Console.WriteLine($" {a}, {b} -> да");
-    }
-    else 
-    {
-        Console.WriteLine($" {a}, {b} -> нет");
-    }
+    res = ($" {a}, {b} -> да");
 }
+Console.WriteLine(res);
