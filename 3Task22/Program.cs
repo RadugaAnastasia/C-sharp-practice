b364ce95422c606c952477f54359e8631336fa2a
Anastasiia <martishka85@list.ru>
2023-06-19 00:28:35 +0300

    add 3task22

---
+++ 3Task22/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1, 4
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+void FindSQ(int n)
+{
+    int i = 1;
+    while(i <= n)
+{
+    Console.Write(i*i + "  ");
+    i ++;
+}
+}
+
+int N = Prompt("Введите число N: ");
+FindSQ(N);
