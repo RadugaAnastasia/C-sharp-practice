392a393bf1c442eacb3755bc7a1536758aff7bb5
Anastasiia <martishka85@list.ru>
2023-06-18 23:41:09 +0300

    add 3task17, 3task18

---
+++ 3Task17/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+//  в которой находится эта точка
+
+//Console.Clear();
+
+int Prompt(string message) //описываем метод, который работает с пользователем
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+void FindQuarter(int X, int Y)
+{
+if(X>0 && Y>0)
+{
+    Console.WriteLine("I четверть");
+}
+else if(X<0 && Y>0)
+{
+    Console.WriteLine("II четверть");
+}
+else if (X<0 && Y<0)
+{
+    Console.WriteLine("III четверть");
+}
+else if (X>0 && Y<0)
+{
+    Console.WriteLine("IV четверть");
+}
+else
+{
+    Console.WriteLine("Ошибка!");
+}
+}
+
+int x = Prompt("Введите координату точки Х: ");
+int y = Prompt("Введите координату точки Y: ");
+FindQuarter(x,y);
---
+++ 3Task18/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y)
+
+int Prompt(string message) //описываем метод, который работает с пользователем
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void FindRange(int n)
+{
+if(n == 1)
+{
+    Console.WriteLine("X>0, Y>0");
+}
+else if(n == 2)
+{
+    Console.WriteLine("X<0, Y>0");
+}
+else if(n == 3)
+{
+    Console.WriteLine("X<0, Y<0");
+}
+else if(n == 4)
+{
+    Console.WriteLine("X>0, Y<0");
+}
+else
+{
+    Console.WriteLine("Ошибка!");
+}
+}
+
+int N = Prompt("Введите номер четверти: ");
+FindRange(N);
