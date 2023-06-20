18f5ab18a5054cddc11cf2f66c6f477cda41e929
Anastasiia <martishka85@list.ru>
2023-06-20 11:32:57 +0300

    add 4Task30

---
+++ 4Task30/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+//  заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while(index < length)
+//     {
+//         collection[index] = new Random().Next(0, 2);
+//         index++;
+//     }
+// }
+
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while(position < count)
+//     {
+//         Console.Write(col[position] + " ");
+//         position++;
+//     }
+// }
+
+// int[]array = new int[8];
+// FillArray(array);
+// PrintArray(array);
+
+
+// Задача 30: Напишите программу, которая 
+// выводит массив из 8 элементов, заполненный 
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+const int ARRAYSIZE = 8;
+int[] randomBinaryArray = RandomBinaryArray();
+PrintArray(randomBinaryArray);
+
+int[] RandomBinaryArray()
+{
+    int[] array = new int[ARRAYSIZE];
+    Random random = new Random();
+
+    for(int i = 0; i < ARRAYSIZE; i++)
+        array[i] = random.Next(0, 2);
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+        Console.Write($"{element} ");
+
+    Console.WriteLine();
+}
