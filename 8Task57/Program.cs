2604e8d4692de4620b48c0fda3e5b4e9358e4190
Anastasiia <martishka85@list.ru>
2023-07-04 11:50:34 +0300

    add 8Task57

---
+++ 8Task57/Program.cs
@@ -0,0 +1,156 @@
+﻿// Задача 57:
+// Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз 
+// встречается элемент входных данных.
+// ВАРИАНТ 1
+const int ROW = 2;
+const int COL = 5;
+const int MIN = 1;
+const int MAX = 9;
+
+int[,] matrix = GetRandomMatrix(ROW, COL, MIN, MAX);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+Dictionary<int, int> counter = ElementCounter(matrix);
+
+foreach(var kvp in counter)
+{
+    Console.WriteLine($"{kvp.Key} встречается {kvp.Value}");
+}
+
+Dictionary<int, int> ElementCounter(int[,] matrix)
+{
+    Dictionary<int, int> counter = new Dictionary<int, int>();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(!counter.ContainsKey(matrix[i, j]))
+                counter.Add(matrix[i, j], 1);
+            else
+                counter[matrix[i, j]]++;
+        }
+    }
+
+    return counter;
+}
+
+int[,] GetRandomMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random random = new Random();
+
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            matrix[i, j] = random.Next(min, max + 1);
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//ВАРИАНТ 2
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = GetArray(rows, columns, -10, 10);
+PrintArray(array);
+Console.WriteLine();
+int[] rowAr = GetRowArray(array);
+SortArray(rowAr);
+Console.WriteLine(String.Join(" ", rowAr));
+PrintData(rowAr);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+int[] GetRowArray(int[,] inArray)
+{
+    int[] result = new int[inArray.Length];
+    int index = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            result[index] = inArray[i, j];
+            index++;
+        }
+    }
+    return result;
+}
+
+void SortArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        for (int j = i + 1; j < inArray.Length; j++)
+        {
+            if (inArray[i] > inArray[j])
+            {
+                int k = inArray[i];
+                inArray[i] = inArray[j];
+                inArray[j] = k;
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintData(int[] inArray)
+{
+    int el = inArray[0];
+    int count = 1;
+    for (int i = 1; i < inArray.Length; i++)
+    {
+        if (inArray[i] != el)
+        {
+            Console.WriteLine($"{el} встречается {count}");
+            el = inArray[i];
+            count = 1;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"{el} встречается {count}");
+}
