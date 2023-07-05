dce54c9dde9cfc8e77e829a98e36739d2e3f2bab
Anastasiia <martishka85@list.ru>
2023-07-05 20:29:26 +0300

    add 7Task49

--- 7Task49/Program.cs
+++ 7Task49/Program.cs
@@ -1,3 +1,62 @@
 ﻿// Задача 49:
 // Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
-// и замените эти элементы на их квадраты.
+// и замените эти элементы на их квадраты. (0 считается четным числом)
+
+
+int rows = Prompt("Введите количество строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+int minValue = Prompt("Введите число для минимального диапазона значений: ");
+int maxValue = Prompt("Введите число для максимального диапазона значений: ");
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+int[,] newarray = FindSqr(array);
+PrintArray(newarray);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+int[,] FindSqr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                arr[i,j] = arr[i,j] * arr[i,j];
+            }
+        }
+    }
+    return arr;
+}
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++) //GetLength(0) - длина строки
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) //GetLength(1) - длина столбцов
+        {
+            Console.Write(array[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
