3d86c177ebdeb02702fda5f7fb4d96202558faca
Anastasiia <martishka85@list.ru>
2023-07-05 20:11:30 +0300

    add 7Task48

---
+++ 7Task48/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 23
+// 1 2 34
+// 2 3 45
+
+int rows = Prompt("Введите количество строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = i + j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++) //GetLength(0) - длина строки
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) //GetLength(1) - длина столбцов
+        {
+            Console.Write(array[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
